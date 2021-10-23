97fa475f304390295af80fa8ec67e4457cf93903
Erofey Bashunov <cannor147@gmail.com>
2021-10-23 13:56:52 +0300

    feat(t02): implemented functions custom Number

---
+++ hw1/src/HW1/T2.hs
@@ -0,0 +1,34 @@
+module HW1.T2 where
+
+import GHC.Num (Natural)
+
+data N = Z | S N
+  deriving Show
+
+nplus :: N -> N -> N
+nplus Z     x = x
+nplus (S x) y = S (nplus x y)
+
+nmult :: N -> N -> N 
+nmult Z         _ = Z
+nmult (S Z)     x = x
+nmult (S (S x)) y = nplus (nmult (S x) y) y 
+
+nsub :: N -> N -> Maybe N
+nsub x     Z     = Just x
+nsub Z     _     = Nothing
+nsub (S x) (S y) = nsub x y
+
+ncmp :: N -> N -> Ordering
+ncmp Z     Z     = EQ
+ncmp (S _) Z     = GT
+ncmp Z     (S _) = LT
+ncmp (S x) (S y) = ncmp x y
+
+nFromNatural :: Natural -> N
+nFromNatural 0 = Z
+nFromNatural n = S $ nFromNatural (n - 1)
+
+nToNum :: Num a => N -> a
+nToNum Z     = 0
+nToNum (S x) = (+ 1) $ nToNum x
